64676da03ffd3980ee570c19ebcc07b464350e33
InfoOrgs <aditya01377@gmail.com>
2023-08-15 23:48:55 +0530

    new updates

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -7,7 +7,7 @@ const corsMiddleware = (req, res, next) => {
     if (allowedOrigins.includes(req.headers.origin)) {
         res.header('Access-Control-Allow-Origin', req.headers.origin);
     }
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 };
